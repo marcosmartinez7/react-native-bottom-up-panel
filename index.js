b7ded1b5ad2403f0b2b78413f76bf420e2f58ac5
marcosmartinez7 <martinez.marcos@live.com>
2018-02-16 21:01:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,246 @@
+import React, { Component } from 'react';
+import {
+    Animated,
+    Dimensions,
+    ScrollView,
+    StyleSheet,
+    TouchableWithoutFeedback,
+    View
+} from 'react-native';
+
+import {Ionicons} from '@expo/vector-icons';
+import {Text} from "native-base";
+import * as Easing from "react-native/Libraries/Animated/src/Easing";
+
+
+const { width, height } = Dimensions.get('window');
+
+/**
+ *  Bottom-up drawer/toggle panel.
+
+ * @author mmartinezciompi@gmail.com
+ *
+ * Description:
+ *
+ * This component is a dynamic height and position view. It has a startHeight property that determines the section of the
+ * view that will be showed (generally the toggle button or header).
+ *
+ * The component has a view with absolute positioning, the position and height of the view is handled by React Native Animations, using an
+ * linear interpolation between the start and end position defined.
+ *
+ * The component has two main parts:
+ *    - header: a transparent view with height = @startHeight and an Icon that will be rotated 180 degrees every time that the panel open/close
+ *    - content: a transparent view to put the content.
+ *
+ * Why are both components transparent?
+ *      Because there are scenarios when you want to show the background of the main view with some opacity. So
+ *      you have to set the right background for your content and header components.
+ *
+ * Why there is no zIndex handling?
+ *      Because the height of the view is dynamic, it starts from the bottom and then covers to topEnd property, so
+ *      the view only covers the part that is visible.
+ *
+ * Props:
+ * - topEnd: distance from top of the screen
+ * - startHeight: part of the hidden view that will be visible
+ * - content: function that renders the panel content inside a scroll view
+ * - icon: function that renders the panel slide icon that will be rotated
+ * - headerText: text that is visible at top of the panel
+ * - headerTextStyle: style applied to that text
+ *
+ * Inspired by   https://github.com/rationalappdev/react-native-bottom-drawer
+
+ */
+
+export default class BottomUpPanel extends Component {
+
+
+    static defaultProps = {
+        isOpen: false
+    };
+
+    // Define state
+    state = {
+        open: false,
+        spinValue: new Animated.Value(0)
+    };
+
+    config = {
+        position: {
+            max: height,
+            start: height - this.props.startHeight,
+            end: this.props.topEnd,
+            min: this.props.topEnd,
+            animates: [
+                () => this._animatedHeight
+            ]
+        },
+        width: {
+            end: width,
+            start: width
+        },
+        height:{
+            end: height,
+            start: this.props.startHeight
+        }
+
+    };
+
+    _animatedHeight = new Animated.Value(this.props.isOpen ? this.config.height.end : this.config.height.start);
+
+
+    _animatedPosition = new Animated.Value(this.props.isOpen
+        ? this.config.position.end
+        : this.config.position.start);
+
+
+    componentWillMount() {
+        this._animatedPosition.addListener((value) => {
+            //Every times that position changes then actualize the related properties. I.e: height, so the view
+            //has the interpolated height
+            this.config.position.animates.map(item => {
+                item().setValue(value.value);
+            })
+        });
+        // Reset value once listener is registered to update depending animations
+        this._animatedPosition.setValue(this._animatedPosition._value);
+    }
+
+
+
+    // Handle isOpen prop changes to either open or close the window
+    componentWillReceiveProps(nextProps) {
+        // isOpen prop changed to true from false
+        if (!this.props.isOpen && nextProps.isOpen) {
+            this.open();
+        }
+        // isOpen prop changed to false from true
+        else if (this.props.isOpen && !nextProps.isOpen) {
+            this.close();
+        }
+    }
+
+    render() {
+        const { content } = this.props;
+
+        // Height according to position
+        let animatedHeight = this._animatedHeight.interpolate({
+            inputRange: [this.config.position.end, this.config.position.start],
+            outputRange: [this.config.height.end, this.config.height.start],
+        });
+
+        // Icon rotation
+        const spin = this.state.spinValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '180deg']
+        });
+        return (
+            <Animated.View style={[styles.buttonUpPanelView, {height: animatedHeight}]}>
+                    <Animated.View
+                        style={[styles.content, {
+                            // Add padding at the bottom to fit all content on the screen
+                            paddingBottom: this.props.topEnd,
+                            width: width,
+                            // Animate position on the screen
+                            transform: [{ translateY: this._animatedPosition }, { translateX: 0 }]
+                        }]}
+                    >
+                        {/*Section for header or button to open the panel*/}
+                        <TouchableWithoutFeedback onPress={()=>{this.toggle()}}>
+                            <Animated.View style={{height:this.props.startHeight}}>
+                                <View style={[this.props.bottomUpSlideBtn, {width: width, height: this.props.startHeight}]}>
+                                    <Animated.View style={{transform: [{rotate: spin}]}}>
+                                        {this.props.icon()}
+                                    </Animated.View>
+                                    <Text style={this.props.headerTextStyle}>{this.props.headerText}</Text>
+                                </View>
+                            </Animated.View>
+                        </TouchableWithoutFeedback>
+
+                        {/* Scrollable content */}
+                        <ScrollView
+                            ref={(scrollView) => { this._scrollView = scrollView; }}
+                            // Enable scrolling only when the window is open
+                            scrollEnabled={this.state.open}
+                            // Hide all scrolling indicators
+                            showsHorizontalScrollIndicator={false}
+                            showsVerticalScrollIndicator={false}
+                            // Trigger onScroll often
+                            scrollEventThrottle={16}                          
+                            onScroll={this._handleScroll}
+                        >
+                            {/* Render content components */}
+                            {content()}
+                        </ScrollView>
+                    </Animated.View>
+                </Animated.View>
+
+        );
+    }
+
+
+    open = () => {
+        this.setState({ open: true }, () => {
+            Animated.timing(this._animatedPosition, {
+                toValue: this.config.position.end,
+                duration: 600,
+            }).start();
+            Animated.timing(
+                this.state.spinValue,
+                {
+                    toValue: 1,
+                    duration: 600,
+                    easing: Easing.linear,
+                    useNativeDriver: true
+                }
+            ).start()
+
+
+        });
+
+    };
+
+    close = () => {
+        this._scrollView.scrollTo({ y: 0 });
+        Animated.timing(this._animatedPosition, {
+            toValue: this.config.position.start,
+            duration: 600,
+        }).start(() => this.setState({
+            open: false,
+        }));
+        Animated.timing(
+            this.state.spinValue,
+            {
+                toValue: 0,
+                duration: 600,
+                easing: Easing.linear,
+                useNativeDriver: true
+            }
+        ).start();
+
+    };
+
+    toggle = () => {
+        if (!this.state.open) {
+            this.open();
+        }
+        else {
+            this.close();
+        }
+    };
+
+   }
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        backgroundColor: 'transparent'
+    },
+    content: {
+        backgroundColor: 'transparent',
+        height: height,
+    },
+    buttonUpPanelView:{position: 'absolute', bottom: 0, width: width,  alignItems: 'center',   justifyContent: 'flex-end',
+        backgroundColor: 'transparent'},
+});
