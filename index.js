ecf8e5e4d471fae0d1bc4828b15a8ac8bd731dca
marcosmartinez7 <martinez.marcos@live.com>
2018-02-16 21:56:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,42 +15,6 @@ import * as Easing from "react-native/Libraries/Animated/src/Easing";
 
 const { width, height } = Dimensions.get('window');
 
-/**
- *  Bottom-up drawer/toggle panel.
-
- * @author mmartinezciompi@gmail.com
- *
- * Description:
- *
- * This component is a dynamic height and position view. It has a startHeight property that determines the section of the
- * view that will be showed (generally the toggle button or header).
- *
- * The component has a view with absolute positioning, the position and height of the view is handled by React Native Animations, using an
- * linear interpolation between the start and end position defined.
- *
- * The component has two main parts:
- *    - header: a transparent view with height = @startHeight and an Icon that will be rotated 180 degrees every time that the panel open/close
- *    - content: a transparent view to put the content.
- *
- * Why are both components transparent?
- *      Because there are scenarios when you want to show the background of the main view with some opacity. So
- *      you have to set the right background for your content and header components.
- *
- * Why there is no zIndex handling?
- *      Because the height of the view is dynamic, it starts from the bottom and then covers to topEnd property, so
- *      the view only covers the part that is visible.
- *
- * Props:
- * - topEnd: distance from top of the screen
- * - startHeight: part of the hidden view that will be visible
- * - content: function that renders the panel content inside a scroll view
- * - icon: function that renders the panel slide icon that will be rotated
- * - headerText: text that is visible at top of the panel
- * - headerTextStyle: style applied to that text
- *
- * Inspired by   https://github.com/rationalappdev/react-native-bottom-drawer
-
- */
 
 export default class BottomUpPanel extends Component {
 
